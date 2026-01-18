8b09b847e1501dd8b9c5d1462514cf20d2f1e655
Deli <A.Delali.Doe@gmail.com>
2026-01-18 06:13:10 -0500

    Delete PolymorphismArtist/Circle.java

--- PolymorphismArtist/Circle.java
+++
@@ -1,9 +0,0 @@
-package week1;
-
-public class Circle extends Shape{
-	//creating method
-	@Override
-	public void drawYourself() {
-		System.out.println("Drawing Circle...loading...Done: ◯");
-	}
-}
