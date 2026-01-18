b17d632c665dadc06070efc6c29edcb852ccba27
Deli <A.Delali.Doe@gmail.com>
2026-01-18 06:13:34 -0500

    Delete PolymorphismArtist/Triangle.java

--- PolymorphismArtist/Triangle.java
+++
@@ -1,9 +0,0 @@
-package week1;
-
-public class Triangle extends Shape{
-	//creating method
-	@Override
-	public void drawYourself() {
-		System.out.println("Drawing Triangle...loading...Done: △");
-	}
-}
