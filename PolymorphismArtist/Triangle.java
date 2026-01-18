24a04cc2fc9aba97bcb39fc0af8ba705b5af313f
Deli <A.Delali.Doe@gmail.com>
2026-01-18 05:33:34 -0500

    Add files via upload

---
+++ PolymorphismArtist/Triangle.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Triangle extends Shape{
+	//creating method
+	@Override
+	public void drawYourself() {
+		System.out.println("Drawing Triangle...loading...Done: △");
+	}
+}
