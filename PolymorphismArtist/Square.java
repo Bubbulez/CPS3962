c22bfd409122cf757288ba3e50a591efbec24c1b
Deli <A.Delali.Doe@gmail.com>
2026-01-18 06:13:42 -0500

    Delete PolymorphismArtist/Square.java

--- PolymorphismArtist/Square.java
+++
@@ -1,9 +0,0 @@
-package week1;
-
-public class Square extends Shape{
-	//creating a method
-	@Override
-	public void drawYourself() {
-		System.out.println("Drawing Square...loading...Done: ▢");
-	}
-}
